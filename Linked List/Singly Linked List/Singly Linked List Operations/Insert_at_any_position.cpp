f21222258eecf8f5787b15c6559c67fb20031926
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-03 20:02:12 +0600

    corner cased handled in the insert at any position

--- Linked List/Singly Linked List/Singly Linked List Operations/Insert_at_any_position.cpp
+++ Linked List/Singly Linked List/Singly Linked List Operations/Insert_at_any_position.cpp
@@ -1,47 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
-        int val;
-        Node* next;
-    Node(int val){
-        this->val=val;
-        this->next=NULL;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-void print_list(Node* head){
-    Node* temp = head;
+void print_list(Node *head)
+{
+    Node *temp = head;
 
     while (temp != NULL)
     {
-        cout<<temp->val<<endl;
+        cout << temp->val << endl;
         temp = temp->next;
     }
-    
 }
 
-void insert_at_any_pos(Node* &head, int idx, int val){
-    Node* newNode = new Node(val);
-    Node* temp = head;
+void insert_at_any_pos(Node *&head, int idx, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
     for (int i = 1; i < idx; i++)
     {
         temp = temp->next;
+        if (temp == NULL)
+            return;
     }
     newNode->next = temp->next;
     temp->next = newNode;
 }
 
-int main(){
-    Node* head = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
 
-    head->next=a;
-    a->next=b;
+    head->next = a;
+    a->next = b;
 
-    insert_at_any_pos(head,2,100);
+    insert_at_any_pos(head, 4, 100);
     print_list(head);
 
     return 0;
