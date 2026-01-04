a2425c08a4ea4422cf49aa0e3659c038cd0b955f
Rakesh Biswas <rbiswas01999@gmail.com>
2026-01-04 19:21:13 +0600

    insert at head updated [head == NULL] -handled

--- Linked List/Singly Linked List/Singly Linked List Operations/Insert_at_head.cpp
+++ Linked List/Singly Linked List/Singly Linked List Operations/Insert_at_head.cpp
@@ -1,45 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
-        int val;
-        Node* next;
-    Node(int val){
-        this->val=val;
-        this->next=NULL;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-void insert_at_head(Node* &head, int val){
-    Node* newNode = new Node(val);
-    newNode->next=head;
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
     head = newNode;
 }
 
-void print_linked_list(Node* head){
-    Node* temp = head;
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
 
     while (temp != NULL)
     {
-        cout<<temp->val<<endl;
-        temp=temp->next;
+        cout << temp->val << endl;
+        temp = temp->next;
     }
 }
 
-int main(){
-    Node* head = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
 
-    head->next=a;
-    a->next=b;
+    head->next = a;
+    a->next = b;
+    b->next = tail;
 
-    insert_at_head(head, 300);
-    insert_at_head(head, 200);
-    insert_at_head(head, 100);
+    insert_at_head(head, tail, 100);
 
     print_linked_list(head);
-    
+
     return 0;
 }
