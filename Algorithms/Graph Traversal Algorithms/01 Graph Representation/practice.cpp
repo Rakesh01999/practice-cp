cc37c9bebdf2f18c23309f86a22c99ecbf1420ba
Rakesh Biswas <rbiswas01999@gmail.com>
2026-06-16 12:32:37 +0600

    graph practice

--- Algorithms/Graph Traversal Algorithms/01 Graph Representation/practice.cpp
+++ Algorithms/Graph Traversal Algorithms/01 Graph Representation/practice.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main(){
     int n, e;cin>>n>>e;
     int adj_mat[n][n];
+    memset(adj_mat, 0, sizeof(adj_mat));
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(i==j) adj_mat[i][j]=1;
-            else adj_mat[i][j]=0;
+            // else adj_mat[i][j]=0;
         }
     }
 
@@ -16,7 +17,7 @@ int main(){
         adj_mat[a][b]=1;
         adj_mat[b][a]=1;
     }
-    
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<adj_mat[i][j]<<" ";
