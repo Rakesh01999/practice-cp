d1fc9497db921b45e37b6329b456d99c07a7c07c
Rakesh Biswas <rbiswas01999@gmail.com>
2026-06-16 11:56:45 +0600

    graph practice

--- Algorithms/Graph Traversal Algorithms/01 Graph Representation/practice.cpp
+++ Algorithms/Graph Traversal Algorithms/01 Graph Representation/practice.cpp
@@ -1,35 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int n, e;cin>>n>>e;
-    int adj_mat [n+1][n+1];
-    
-    memset(adj_mat, 0, sizeof(adj_mat));
-
-    for (int i = 1; i <= n; i++)
-    {
-        for(int j=1; j<=n ; j++){
-            if(i == j)
-                adj_mat [i][j] = 1;
+    int adj_mat[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) adj_mat[i][j]=1;
+            else adj_mat[i][j]=0;
         }
     }
-    
-    while (e--)
-    {
-        int a, b;cin>>a>>b;
-        adj_mat[a][b] = 1;
-        adj_mat[b][a] = 1;
-    }
-    
-    for (int i = 1; i <= n; i++)
-    {
-        for(int j=1; j<=n ; j++){
-                cout<<adj_mat [i][j] << " ";
-        }cout<<endl;
-    }
 
-    return 0;
+    for(int i=0;i<e;i++){
+        int a, b;cin>>a>>b;
+        adj_mat[a][b]=1;
+        adj_mat[b][a]=1;
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<adj_mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
 
 // input:
