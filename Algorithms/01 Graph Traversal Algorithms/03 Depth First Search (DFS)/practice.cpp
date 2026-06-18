a7644d5bbe7197ca7f6db5113ec9a1fda33cbbe0
Rakesh Biswas <rbiswas01999@gmail.com>
2026-06-18 22:52:34 +0600

    practice

--- Algorithms/01 Graph Traversal Algorithms/03 Depth First Search (DFS)/practice.cpp
+++ Algorithms/01 Graph Traversal Algorithms/03 Depth First Search (DFS)/practice.cpp
@@ -1,42 +1,78 @@
-
 #include <bits/stdc++.h>
 using namespace std;
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
 
-vector<int> adj_list[105];
-int vis[105];
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return false;
+    }
+    return true;
+}
 
-void dfs(int src){
-    cout<<src<<" ";
-    vis[src] = true;
-    for(int child:adj_list[src]){
-        if(!vis[child]){
-            dfs(child);
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        // cout<<ci<<" "<<cj<<endl;
+        if (valid(ci, cj) && !vis[ci][cj])
+            dfs(ci, cj);
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
         }
     }
-}
-
-int main(){
-    int n, e;cin>>n>>e;
-    while(e--){
-        int a, b;cin>>a>>b;
-        adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
+    cout << "output:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << grid[i][j];
+        }
+        cout << endl;
     }
-    memset(vis, false, sizeof(vis));
-    dfs(0);
 
-    return 0;   
+    // memset(vis, false, sizeof(vis));
+    // int si, sj;
+    // cin >> si >> sj;
+    // dfs(si, sj);
+
+    return 0;
 }
 
-// Input:
-// 7 7
-// 0 1
-// 0 2
-// 0 3
-// 1 4
-// 4 5
-// 5 2
-// 3 6
+// input :
+// 3 4
+// ....
+// ....
+// ....
+// 1 2
 
-// Output:
-// 0 1 4 5 2 3 6
+// output:
+// 1 2
+// 0 2
+// 0 1
+// 1 1
+// 2 1
+// 2 0
+// 1 0
+// 0 0
+// 2 2
+// 2 3
+// 1 3
+// 0 3
