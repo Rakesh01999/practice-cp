0e1d131dfc4732c5fec5af044125bbbca84ad741
Rakesh Biswas <rbiswas01999@gmail.com>
2026-06-21 10:48:51 +0600

    BFS, DFS on grid, traverse -practiced

--- Algorithms/01 Graph Traversal Algorithms/3.5 Practice Day 2/practice.cpp
+++ Algorithms/01 Graph Traversal Algorithms/3.5 Practice Day 2/practice.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 char grid[105][105];
 bool vis[105][105];
+queue<pair<int, int>>q;
 vector<pair<int, int>>d={{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 int n, m;
-queue<pair<int, int>>q;
+int level[105][105];
 
 bool valid(int ci, int cj){
-    if(ci<0 || cj<0 || ci>=n || cj>=m)
+    if(ci<0 || ci>=n || cj<0 || cj>=m)
         return false;
     return true;
 }
@@ -16,25 +16,30 @@ bool valid(int ci, int cj){
 void bfs(int si, int sj){
     q.push({si, sj});
     vis[si][sj]=true;
-    while(!q.empty()){
+    level[si][sj]=0;
+    while (!q.empty())
+    {
         // node ber koro
-        int par_i=q.front().first;
-        int par_j=q.front().second;
+        int pi=q.front().first;   
+        int pj=q.front().second;   
         q.pop();
-
+        
         // node niye kaj
-        cout<<par_i<<" "<<par_j<<endl;
+        // cout<<pi<<" "<<pj<<endl;
 
         // child push
         for(int i=0;i<4;i++){
-            int ci=par_i + d[i].first;
-            int cj=par_j + d[i].second;
+            int ci=pi+d[i].first;
+            int cj=pj+d[i].second;
+
             if(valid(ci, cj) && !vis[ci][cj]){
                 q.push({ci, cj});
                 vis[ci][cj]=true;
+                level[ci][cj]=level[pi][pj]+1;
             }
         }
     }
+    
 }
 
 int main(){
@@ -44,27 +49,37 @@ int main(){
             cin>>grid[i][j];
         }
     }
+
     memset(vis, false, sizeof(vis));
-    // cout<<"output:"<<endl;
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<m;j++){
-    //         cout<<grid[i][j]<<" ";
-    //     }cout<<endl;
-    // }
-    int si, sj;cin>>si>>sj;
+    memset(level, -1, sizeof(level));
+    int si, sj, di, dj;
+    cin>>si>>sj;
+    cin>>di>>dj;
     bfs(si, sj);
+    cout<<level[di][dj]<<endl;
+
     return 0;
 }
 
 
-
-
-// input:
+// input :
 // 3 4
 // ....
 // ....
 // ....
 // 1 2
+// 2 0
 
-// output : 
-
+// output:
+// 1 2
+// 0 2
+// 0 1
+// 1 1
+// 2 1
+// 2 0
+// 1 0
+// 0 0
+// 2 2
+// 2 3
+// 1 3
+// 0 3
