93ec6d53ba8c23883ef8d15fd8334eb2c4483407
Rakesh Biswas <rbiswas01999@gmail.com>
2026-06-22 13:36:30 +0600

    5_4 Max area of Island

---
+++ Algorithms/02 Cycle Detection & Dijkstra/05 Problem Solving with BFS and DFS/Max_Area_of_Island.cpp
@@ -0,0 +1,65 @@
+/*
+
+Intuition
+Traverse the grid, and whenever we find an unvisited land cell, start a DFS/BFS to calculate its area.
+Keep track of the maximum area encountered.
+Approach
+Use a visited matrix to avoid revisiting cells.
+
+For each unvisited land cell, perform DFS to count the connected 1’s (area).
+
+Update the maximum area after finishing each island.
+
+Return the maximum at the end.
+
+Complexity
+Time complexity:
+O(N*M)
+
+Space complexity:
+O(N*M)
+
+*/
+
+class Solution {
+public:
+    bool vis[55][55];
+    vector<pair<int,int>> d =  {{0,-1},{0,1},{-1,0},{1,0}};
+    int n,m,cnt,mx;
+
+    bool valid(int i, int j)
+    {
+        if(i<0 || i>=n || j<0 || j>=m)
+            return false;
+        return true;
+    }
+
+    void dfs(int si, int sj, vector<vector<int>>& grid)
+    {
+        vis[si][sj] = true;
+        cnt++;
+        for(int i=0;i<4;i++)
+        {
+            int ci = si + d[i].first;
+            int cj = sj + d[i].second;
+            if(valid(ci,cj) && !vis[ci][cj] && grid[ci][cj]==1)
+                dfs(ci,cj,grid);
+        }
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        mx = 0;
+        memset(vis,false,sizeof(vis));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(!vis[i][j] && grid[i][j]==1)
+                {
+                    cnt = 0;
+                    dfs(i,j,grid);
+                    mx = max(cnt,mx);
+                }
+        return mx;
+    }
+};
