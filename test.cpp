3f88f81d3bf8e38fed982cf1ac98d865d01bd4c5
Rakesh Biswas <rbiswas01999@gmail.com>
2026-07-08 10:13:37 +0600

    dynamic node creatino in singly linked-list

--- test.cpp
+++ test.cpp
@@ -12,9 +12,20 @@ class Node{
 };
 
 int main(){
-    Node a(10), b(20), c(30);
-    a.next=&b;
-    b.next=&c;
+    // Node a(10), b(20), c(30);
+    Node* head=new Node(10);
+    Node* a=new Node(30);
+    Node* b=new Node(40);
+    Node* c=new Node(50);
+    
+    head->next=a;
+    a->next=b;
+    b->next=c;
 
-    cout<<a.val<<" "<<b.val<<" "<<c.val<<endl;
+    // cout<<head->val<<" "<<a->val<<" "<<b->val<<" "<<c->val<<endl;
+    cout<<head->val<<" ";
+    cout<<head->next->val<<" ";
+    cout<<head->next->next->val<<" ";
+    cout<<head->next->next->next->val<<" ";
+
 }
