36ea10dde6b8d3ebcb3d4104a3a59c39a0d2cfec
Rakesh Biswas <rbiswas01999@gmail.com>
2026-07-08 10:01:15 +0600

    Linked List practiced -node creation with constructor

--- test.cpp
+++ test.cpp
@@ -5,21 +5,16 @@ class Node{
     public:
         int val;
         Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
 };
 
 int main(){
-    Node a, b, c;
-    a.val=10;
-    b.val=20;
-    c.val=30;
-    
+    Node a(10), b(20), c(30);
     a.next=&b;
     b.next=&c;
-    c.next=NULL;
 
-    // cout<<a.val<<" "<<b.val<<" "<<c.val<<endl;
-    cout<<a.val<<" ";
-    cout<<(*a.next).val<<" ";
-    cout<<(*a.next).val<<" ";
-    
+    cout<<a.val<<" "<<b.val<<" "<<c.val<<endl;
 }
