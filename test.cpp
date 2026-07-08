e4269638b90342d45cc766f6d9022e9a94f2a6d2
Rakesh Biswas <rbiswas01999@gmail.com>
2026-07-08 09:57:07 +0600

    node creation in linked list

--- test.cpp
+++ test.cpp
@@ -1,60 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Node{
     public:
         int val;
         Node* next;
-    Node(int val){
-        this->val = val;
-        this->next = NULL;
-    }
 };
 
-void insert_at_tail(Node* &head, Node* &tail, int val){
-    Node* newNode = new Node(val);
-    if(head == NULL)
-    {
-        head = newNode;
-        tail = newNode;
-        return;
-    }
-    tail->next = newNode;
-    tail = newNode ;
+int main(){
+    Node a, b, c;
+    a.val=10;
+    b.val=20;
+    c.val=30;
+    
+    a.next=&b;
+    b.next=&c;
+    c.next=NULL;
+
+    // cout<<a.val<<" "<<b.val<<" "<<c.val<<endl;
+    cout<<a.val<<" ";
+    cout<<(*a.next).val<<" ";
+    cout<<(*a.next).val<<" ";
+    
 }
-
-// void print_list(Node* head){
-//     Node* temp = head;
-//     while(temp != NULL)
-//     {
-//         cout<<temp->val<<endl;
-//         temp=temp->next;
-//     }
-// }
-
-void print_list(Node* temp){
-    if(temp == NULL)
-    {
-        return ;
-    }
-    print_list(temp->next);
-    cout<<temp->val<<endl;
-}
-
-int main()
-{
-    Node* head =NULL;
-    Node* tail =NULL;
-
-    int val;
-    while (true)
-    {
-        cin>>val;
-        if(val == -1)   
-            break;
-        insert_at_tail(head, tail, val);
-    }
-    print_list(head);    
-    return 0;
-}
-
