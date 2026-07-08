74860282922b528c6ae7b9c07cf9a3ffdab92130
Rakesh Biswas <rbiswas01999@gmail.com>
2026-07-08 22:12:32 +0600

    singly linked list practiced

--- test.cpp
+++ test.cpp
@@ -11,7 +11,17 @@ class Node{
     }
 };
 
-void insert_in_list(Node* &head, Node* &tail, int val){
+void insert_at_head(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+    }
+    newNode->next=head;
+    head=newNode;
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
     Node* newNode=new Node(val);
     if(head==NULL){
         head=newNode;
@@ -20,6 +30,23 @@ void insert_in_list(Node* &head, Node* &tail, int val){
     }
     tail->next=newNode;
     tail=newNode;
+}
+
+void insert_at_any_pos(Node* &head, Node* & tail, int idx, int val){
+    Node* newNode=new Node(val);
+    Node* temp=head;
+    for(int i=1;i<idx;i++){
+        if(temp==NULL)
+            break;
+        temp=temp->next;
+    }
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+        return ;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
 }
 
 void print_list(Node* head){
@@ -38,6 +65,15 @@ void print_list_rev(Node* temp){
     cout<<temp->val<<" ";
 }
 
+void sort_list(Node* head){
+    for(Node* i=head;i!=NULL;i=i->next){
+        for(Node* j=i->next;j!=NULL;j=j->next){
+            if(i->val>j->val)
+                swap(i->val, j->val);
+        }
+    }
+}
+
 int main(){
     Node* head=NULL;
     Node* tail=NULL;
@@ -47,9 +83,12 @@ int main(){
         cin>>val;
         if(val==-1)
             break;
-        insert_in_list(head, tail, val);
+        insert_at_tail(head, tail, val);
     }
+    // insert_at_head(head, tail, 100);
+    insert_at_any_pos(head, tail, 2, 100);
+    // sort_list(head);
     print_list(head);
-    print_list_rev(head);
+    // print_list_rev(head);
     
 }
