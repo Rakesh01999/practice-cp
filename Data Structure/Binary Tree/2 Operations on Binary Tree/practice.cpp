bed85f6a73fe91eb353526ac166225074311b18a
Rakesh Biswas <rbiswas01999@gmail.com>
2026-07-19 21:24:32 +0600

    binary tree input practice

--- Data Structure/Binary Tree/2 Operations on Binary Tree/practice.cpp
+++ Data Structure/Binary Tree/2 Operations on Binary Tree/practice.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int cnt = 0;
 
 class Node{
     public:
@@ -17,73 +16,58 @@ class Node{
 Node* input_tree(){
     int val;cin>>val;
     Node* root;
-    if(val == -1) root = NULL;
-    else root = new Node(val);
-    queue<Node* >q;
-    q.push(root);
-    while (!q.empty())
-    {
-        Node* f = q.front();
+    queue<Node*>q;
+    if(val==-1) return NULL;
+    else root=new Node(val);
+    if(root) q.push(root);
+    while(!q.empty()){
+        // Node ber koro
+        Node* f=q.front();
         q.pop();
 
-        int l,r;cin>>l>>r;
-        Node* myLeft, *myRight;
-        if(l == -1) myLeft = NULL;
-        else myLeft = new Node(l);
-        if(r == -1) myRight = NULL;
-        else myRight = new Node(r);
+        // Node niye kaj
+        int l, r;cin>>l>>r;
+        Node* myLeft;
+        Node* myRight;
+        if(l==-1) myLeft=NULL;
+        else myLeft=new Node(l);
+        
+        if(r==-1) myRight=NULL;
+        else myRight=new Node(r);
 
-        f->left = myLeft;
-        f->right = myRight;
+        f->left=myLeft;
+        f->right=myRight;
 
-        if(f->left) q.push(f->left);
-        if(f->right) q.push(f->right);
+        // Node er child push
+        if(f->left) q.push(myLeft);
+        if(f->right) q.push(myRight);
     }
     return root;
 }
 
-// void level_order(Node* root){
-//     if(root == NULL){
-//         cout<<"No Tree !"<<endl;
-//         return;
-//     }
-//     queue<Node* >q;
-//     q.push(root);
-//     while (!q.empty())
-//     {
-//         Node* f = q.front();
-//         q.pop();
-//         cout<<f->val<<" ";
-//         cnt++;
-//         if(f->left) q.push(f->left);
-//         if(f->right) q.push(f->right);
-//     }
-// }
+void level_order(Node* root){
+    if(root==NULL){
+        cout<<"No Tree"<<endl;
+        return ;
+    }
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        // Node ber koro
+        Node* f=q.front();
+        q.pop();
 
-// int count_nodes(Node* root){
-//     if(root == NULL)
-//         return 0;
-//     int l = count_nodes(root->left);
-//     int r = count_nodes(root->right);
-//     return l+r+1;
-// }
-
-int count_leaf_nodes(Node* root){
-    if(root == NULL)
-        return 0;
-    if(root->left == NULL && root->right == NULL)
-        return 1;
-    int l = count_leaf_nodes(root->left);
-    int r = count_leaf_nodes(root->right);
-    return l+r;
+        // Node niye kaj
+        cout<<f->val<<endl;
+        
+        // Child push
+        if(f->left)q.push(f->left);
+        if(f->right)q.push(f->right);
+    }
 }
 
 int main(){
-    Node* root = input_tree();
-    // level_order(root);
-    // cout<<endl<<"No of nodes :"<<cnt<<endl;
-    // cout<<"No of nodes :"<<count_nodes(root)<<endl;
-    cout<<"No of nodes :"<<count_leaf_nodes(root)<<endl;
-
+    Node* root=input_tree();
+    level_order(root);
     return 0;
 }
